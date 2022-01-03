d30b18ae419e70d505498945f02406b3802e6562
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-01-03 21:24:28 +0100

    add to array onClick fn

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,16 +1,45 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/no-array-index-key */
-// import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from '../button/button';
 import { gameData } from '../../helpers/game';
 import styles from './game.module.scss';
 
+interface State {
+  value?: any;
+  checked?: boolean;
+}
+
 const Game = () => {
   const { t } = useTranslation();
 
-  const min = 0;
-  const max = gameData.length;
-  const number = Math.floor(Math.random() * (max - min) + min);
+  const [randomNumber, setRandomNumber] = useState(0);
+
+  useEffect(() => {
+    const min = 0;
+    const max = gameData.length;
+    const number = Math.floor(Math.random() * (max - min) + min);
+
+    setRandomNumber(number);
+  }, []);
+
+  const [wordsList, setWordsList] = useState<State[]>([]);
+
+  const toggleClick = (word: any) => {
+    if (wordsList.includes(word)) {
+      const index = wordsList.findIndex((el) => el === word);
+      wordsList.splice(index, 1);
+      setWordsList(wordsList);
+    } else {
+      setWordsList([...wordsList, word]);
+    }
+  };
+
+  useEffect(() => {
+    console.log(wordsList);
+  }, [wordsList]);
 
   const toggleCheck = () => {
     console.log('check');
@@ -20,8 +49,8 @@ const Game = () => {
     <div className={styles.game}>
       <h2 className={styles.game__title}>{t('game.title')} #variable</h2>
       <div className={styles.game__box}>
-        {gameData[number].all_words.map((word: string, index) => (
-          <p key={index} className={styles.game__boxWord}>
+        {gameData[randomNumber].all_words.map((word: string, index) => (
+          <p key={index} className={styles.game__boxWord} onClick={() => toggleClick(word)}>
             {word}
           </p>
         ))}
