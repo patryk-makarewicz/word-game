964c801403b399fc957d459069d82e4cf42a3eaa
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-01-05 10:26:01 +0100

    add fn add to checked

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/no-array-index-key */
 import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from '../button/button';
@@ -16,6 +16,8 @@ const Game = () => {
   const { t } = useTranslation();
 
   const [randomNumber, setRandomNumber] = useState(0);
+  const [wordsList, setWordsList] = useState<State[]>([]);
+  const newWordList = [...wordsList];
 
   useEffect(() => {
     const min = 0;
@@ -23,23 +25,23 @@ const Game = () => {
     const number = Math.floor(Math.random() * (max - min) + min);
 
     setRandomNumber(number);
+
+    const fetchWords = gameData[randomNumber].all_words;
+    const data: { value: string; checked: boolean }[] = [];
+    fetchWords.forEach((elem) => {
+      data.push({
+        value: elem,
+        checked: false,
+      });
+    });
+    setWordsList(data);
   }, []);
 
-  const [wordsList, setWordsList] = useState<State[]>([]);
-
-  const toggleClick = (word: any) => {
-    if (wordsList.includes(word)) {
-      const index = wordsList.findIndex((el) => el === word);
-      wordsList.splice(index, 1);
-      setWordsList(wordsList);
-    } else {
-      setWordsList([...wordsList, word]);
-    }
+  const toggleClick = (wordValue: any) => {
+    const index = wordsList.findIndex((word) => word.value === wordValue);
+    newWordList[index].checked = !newWordList[index].checked;
+    setWordsList(newWordList);
   };
-
-  useEffect(() => {
-    console.log(wordsList);
-  }, [wordsList]);
 
   const toggleCheck = () => {
     console.log('check');
@@ -49,9 +51,13 @@ const Game = () => {
     <div className={styles.game}>
       <h2 className={styles.game__title}>{t('game.title')} #variable</h2>
       <div className={styles.game__box}>
-        {gameData[randomNumber].all_words.map((word: string, index) => (
-          <p key={index} className={styles.game__boxWord} onClick={() => toggleClick(word)}>
-            {word}
+        {wordsList.map((word: any, index) => (
+          <p
+            key={index}
+            className={word.checked ? styles.game__boxWordChecked : styles.game__boxWord}
+            onClick={() => toggleClick(word.value)}
+          >
+            {word.value}
           </p>
         ))}
       </div>
