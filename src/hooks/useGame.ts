0eb346257d11d6c02c8f27a18c02e5903b9bebc7
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2022-11-15 22:46:00 +0100

    Create hook

---
+++ src/hooks/useGame.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import { useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useQuery } from 'react-query';
+
+import { QueryKey, Game } from '../api';
+import { GameModel } from '../api/Game/Game.dto';
+
+export const useRepoList = () => {
+  const { t } = useTranslation();
+  const [initialLoading, setInitialLoading] = useState(true);
+
+  const { data, isLoading, isError } = useQuery<GameModel>([QueryKey.loadGame], Game.getGame, {
+    refetchOnWindowFocus: false,
+    onSettled: () => {
+      setInitialLoading(false);
+    },
+    onError: () => {
+      console.log(t('messages.fail.generic'));
+    },
+  });
+
+  return {
+    data: useMemo(() => data || [], [data]),
+    isLoading: isLoading || initialLoading,
+    isError,
+  };
+};
